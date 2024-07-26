9676eef63ec6966b8932a8772219a3455c3bc8f7
kevin-mboni <kevin.mbonigaba@amalitechtraining.org>
2024-07-26 16:37:09 +0200

    Add project jar file

--- web-sorting-algorithm/src/main/java/com/websortingalgorithm/web_sorting_algorithm/controller/SortingViewController.java
+++ web-sorting-algorithm/src/main/java/com/websortingalgorithm/web_sorting_algorithm/controller/SortingViewController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,7 +21,7 @@ public class SortingViewController {
 
     @GetMapping("/view")
     public String index() {
-        return "index"; // Return the view name for the initial page load
+        return "index";
     }
 
     @PostMapping("/sort")
